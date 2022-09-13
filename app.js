f97a50de85ab12158915c4a337f047c691d60b4d
나상준 <sangjoonna@nasangjun-ui-MacBookPro.local>
2022-09-13 20:57:58 +0900

    const let var

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("hello");
+const a = 5;
+const b = 2;
+let myName = "sangjoon";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "nasangjoon";
+
+console.log("your new name is " + myName);
